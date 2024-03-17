a1e2490d56eeca0beb04ed0c7cb5375ceb20a806
Kylix Eza S <ksaputra@dopalearn.com>
2024-03-17 15:53:58 +0700

    fix(recipe): changed time zone to asia jakarta (TODO included)

--- recipe/src/main/kotlin/util/PreferedConsumeAtConverter.kt
+++ recipe/src/main/kotlin/util/PreferedConsumeAtConverter.kt
@@ -4,10 +4,12 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import java.time.ZoneId
 
 fun getPreferConsumeAt(): PreferConsumeAt {
     val now = Clock.System.now()
-    val currentTime = now.toLocalDateTime(TimeZone.currentSystemDefault()).time
+    //TODO: Make sure to change the timezone to the user's timezone using global ip address to detect the timezone
+    val currentTime = now.toLocalDateTime(TimeZone.of("Asia/Jakarta")).time
     return when (currentTime) {
         in LocalTime(5, 0)..LocalTime(10, 0) -> PreferConsumeAt.BREAKFAST
         in LocalTime(10, 0)..LocalTime(14, 0) -> PreferConsumeAt.LUNCH
