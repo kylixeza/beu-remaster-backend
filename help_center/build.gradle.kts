916dc11b1b06591d8c9c5f5148b000a44252a604
Kylix Eza S <ksaputra@dopalearn.com>
2024-03-27 21:15:27 +0700

    feat(help_center): created help center to send email

---
+++ help_center/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version Version.kotlinVersion
+    id("io.ktor.plugin") version Version.ktorVersion
+}
+
+group = App.group
+version = App.version
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(Libs.Ktor.core)
+    implementation(Libs.Exposed.core)
+    implementation(Libs.Util.jnanoid)
+    implementation(Libs.Util.dateTime)
+
+    implementation(Libs.Koin.koin)
+
+    api(project(Modules.middleware))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
