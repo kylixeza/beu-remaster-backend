0d42ba7b9957becea58c2d741c4436b1145e3955
Kylix Eza S <ksaputra@dopalearn.com>
2025-05-04 13:28:55 +0700

    refactor(recognition): revamp prompt and related recipes queries

--- recipe/src/main/kotlin/repository/recognition/RecognitionRepositoryImpl.kt
+++ recipe/src/main/kotlin/repository/recognition/RecognitionRepositoryImpl.kt
@@ -1,4 +1,4 @@
-package repository.prediction
+package repository.recognition
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import database.DatabaseFactory
@@ -11,20 +11,22 @@ import open_ai.*
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.lowerCase
 import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import storage.CloudStorageService
 import tables.CategoryRecipeTable
+import tables.CategoryTable
 import tables.PredictionResultTable
 import tables.RecipeTable
 import util.createTimeStamp
 import util.toRecipeListResponse
 import java.util.*
 
-class PredictionRepositoryImpl(
+class RecognitionRepositoryImpl(
     private val db: DatabaseFactory,
     private val cloudStorageService: CloudStorageService,
     private val openApiService: OpenApiService
-): PredictionRepository {
-    override suspend fun insertPredictionResult(request: PredictionResultRequest, fileByte: ByteArray) {
+): RecognitionRepository {
+    override suspend fun insertRecognitionResult(request: PredictionResultRequest, fileByte: ByteArray) {
         db.dbQuery {
             val url = cloudStorageService.run { fileByte.uploadFile("prediction/${request.prediction}") }
 
@@ -39,19 +41,33 @@ class PredictionRepositoryImpl(
         }
     }
 
-    override suspend fun getRelatedRecipes(uid: String, query: String): List<RecipeListResponse> {
+    override suspend fun getRelatedRecipes(
+        uid: String,
+        food: String,
+        category: String?,
+    ): List<RecipeListResponse> {
         return db.dbQuery {
             val recipesByName = getBaseQuery().select {
-                RecipeTable.name.lowerCase().like("%$query%".lowercase(Locale.getDefault()))
+                RecipeTable.name.lowerCase().like("%$food%".lowercase(Locale.getDefault()))
             }.getBaseGroupBy().map { it.toRecipeListResponse(uid) }
 
-            val categoryIds = CategoryRecipeTable.select {
-                CategoryRecipeTable.recipeId inList recipesByName.map { it.recipeId }
-            }.map { it[CategoryRecipeTable.categoryId] }.distinctBy { it }
+            val recipeIdsByCategory = if (category == null) {
+                val categoryIds = CategoryRecipeTable.select {
+                    CategoryRecipeTable.recipeId inList recipesByName.map { it.recipeId }
+                }.map { it[CategoryRecipeTable.categoryId] }.distinctBy { it }
 
-            val recipeIdsByCategory = CategoryRecipeTable.select {
-                CategoryRecipeTable.categoryId inList categoryIds
-            }.map { it[CategoryRecipeTable.recipeId] }
+                CategoryRecipeTable.select {
+                    CategoryRecipeTable.categoryId inList categoryIds
+                }.map { it[CategoryRecipeTable.recipeId] }
+            } else {
+                val categoryId = CategoryTable.select {
+                    CategoryTable.name.lowerCase() eq category.lowercase()
+                }.map { it[CategoryTable.categoryId] }.firstOrNull().orEmpty()
+
+                CategoryRecipeTable.select {
+                    CategoryRecipeTable.categoryId eq categoryId
+                }.map { it[CategoryRecipeTable.recipeId] }
+            }
 
             val recipesByCategory = getBaseQuery().select {
                 RecipeTable.recipeId inList recipeIdsByCategory
@@ -61,7 +77,14 @@ class PredictionRepositoryImpl(
         }
     }
 
-    override suspend fun classifyImage(uid: String, fileBytes: ByteArray): String {
+    override suspend fun recognizeImage(
+        uid: String,
+        fileBytes: ByteArray,
+    ): String {
+        val availableCategories = db.dbQuery {
+            CategoryTable.selectAll().map { it[CategoryTable.name] }
+        }
+
         val base64Image = Base64.getEncoder().encodeToString(fileBytes)
         val request = ChatRequest(
             model = "gpt-4.1-mini",
@@ -71,9 +94,19 @@ class PredictionRepositoryImpl(
                     content = listOf(
                         ChatMessageContent(
                             type = "text",
-                            text = "Describe what this picture is. Answer directly, no explanation, no period, and use the case format Aaaa Bbbb. " +
-                                    "The result must be translated into Indoensian. " +
-                                    "If it is not classified as an image of food, return the words Not A Food. "
+                            text =
+                            """
+                                Describe what this picture is. Answer directly, no explanation, no period, and use the case format Aaaa Bbbb. 
+                                The result must be translated into Indonesian. 
+                                If it is not classified as an image of food, return the words Not A Food. But do not translate Not A Food into Indonesian. 
+                                Next, from these existing categories: ${availableCategories.joinToString(", ")} which one has the closest meaning to the image? 
+                                Return the result into this json format
+                                {
+                                    "result": "Aaaa Bbbb",
+                                    "closest_category": "category"
+                                }
+                            """.trimIndent()
+
                         ),
                         ChatMessageContent(
                             type = "image_url",
